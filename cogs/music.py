ff9d7807b8a83964a1e80bde838be1184b7a88a1
TypicalWolf <ihsanbinsajid@gmail.com>
2021-11-09 12:20:55 +0530

    PauseCMDFixed

--- cogs/music.py
+++ cogs/music.py
@@ -355,7 +355,7 @@ class Music(commands.Cog):
     async def _pause(self, ctx: commands.Context):
         """Pauses the currently playing song."""
 
-        if not ctx.voice_state.is_playing and ctx.voice_state.voice.is_playing():
+        if ctx.voice_state.is_playing and ctx.voice_state.voice.is_playing():
             ctx.voice_state.voice.pause()
             await ctx.message.add_reaction('⏯')
 
