abadc518a0b78f95b21b70d0b876cd9905ed3528
TypicalWolf <ihsanbinsajid@gmail.com>
2022-01-05 15:50:51 +0530

    NuKeCmD

--- cogs/nuke.py
+++ cogs/nuke.py
@@ -8,16 +8,13 @@ class Dangerous_shit(commands.Cog):
     def __init__(self, client):
         self.client = client
     
-    @commands.command(name="ping")
+    @commands.command(name="Nuke")
     async def nuke(self, ctx):
         """Nuke the server..."""
         
         await ctx.send("https://tenor.com/view/rick-roll-rick-ashley-never-gonna-give-you-up-gif-22113173")
 
     
-    # @commands.command()
-    # async def ping(self, ctx):
-    #     await ctx.send(f':ping_pong: | Latency is : {round(command.latency * 1000)}ms')
-
 def setup(client):
     client.add_cog(Dangerous_shit(client))
+
