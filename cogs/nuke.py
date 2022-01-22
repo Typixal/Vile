ae8d7d9500f023581b44e07373c2f6a9304f330c
TypicalWolf <ihsanbinsajid@gmail.com>
2022-01-22 19:17:11 +0530

    NukeCMDUpdate

--- cogs/nuke.py
+++ cogs/nuke.py
@@ -8,11 +8,12 @@ class Dangerous_shit(commands.Cog):
     def __init__(self, client):
         self.client = client
     
-    @commands.command(name="Nuke")
+    
+    @commands.command(name="nuke")
     async def nuke(self, ctx):
         """Nuke the server..."""
         
-        await ctx.send("https://tenor.com/view/rick-roll-rick-ashley-never-gonna-give-you-up-gif-22113173")
+        await ctx.send("https://tenor.com/view/who-the-hell-do-you-think-youre-talking-to-dog-talking-who-do-you-think-you-are-gif-13502177")
 
     
 def setup(client):
